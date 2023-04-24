0bfad40803132e437eaf9554d71bac1adefb049d
Jessica Zelasky <jzelasky@gmail.com>
2023-04-24 10:42:40 -0400

    added footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className='font-1 mt-5 p-3 d-flex justify-content-around'>
+      <a href="https://github.com/jzelasky">Github</a>
+      <a href="https://linkedin.com/in/jessica-zelasky">LinkedIn</a>
     </div>
   );
 }

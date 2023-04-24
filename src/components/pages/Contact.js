eee63568710d4cbf321bf9ab684313544eb32a33
Jessica Zelasky <jzelasky@gmail.com>
2023-04-24 10:21:20 -0400

    added contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
-      
+    <div className="formStyles">
+      <h1 className="m-3 formStyles">Contact</h1>
+      <form className='m-3'>
+        <div className='form-group formStyles'>
+          <label for="inputName">Name</label>
+          <input className='form-control' id='inputName' placeholder="Enter Name"></input>
+        </div>
+        <div className='form-group formStyles mt-3'>
+          <label for="inputEmail">Email address</label>
+          <input type="email" className='form-control' id='inputEmail' placeholder="Enter Email"></input>
+        </div>
+        <div className='form-group formStyles mt-3'>
+          <label for="inputMessage">Message</label>
+          <input className='form-control' id='inputMessage' placeholder="Enter Message" ></input>
+        </div>
+        <button type="submit" className="btn btn-primary mt-3">Submit</button>
+      </form>
     </div>
   );
 }

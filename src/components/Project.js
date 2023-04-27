d895aee3382637a2209f9fe75b2b508e0cd43c60
Jessica Zelasky <jzelasky@gmail.com>
2023-04-27 09:17:19 -0400

    worked on portfolio page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-export default function Project(ex) {
-    return <div>{ex}</div>
+export default function Project(title, desc, link, github, img) {
+    return (
+       <div className="card m-3 bg-warning">
+            <img className='card-img-top' src={img} alt='project screenshot'></img>
+            <div className='card-body'>
+                <h5 className='card-title'>{title}</h5>
+                <p className='card-text'>{desc}</p>
+            </div>
+            <div className='card-body'>
+                <a href={link} className='card-link'>Deployed Webpage</a>
+                <a href={github} className='card-link'>Github Repository</a>
+            </div>
+       </div>
+    )
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Project from '../Project';
 
-const ex = "hello"
-const projects = ['one', 'two', 'three']
+const projects = [
+  ["title1", "desc1", "link1", "github1", "img1"],
+  ["title2", "desc2", "link1", "github1", "img1"]
+]
 
 export default function Portfolio(props) {
   console.log(props)
   
   return (
-    <div>
-      {projects.map((item) => (
-        Project(item)
-      ))}
+    <div className='m-5'>
+      {projects.map((item) => (Project(item[0], item[1], item[2], item[3], item[4], item[5])))}
     </div>
   )
   
